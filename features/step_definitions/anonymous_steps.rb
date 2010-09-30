87b8af3c2a69df897f0cef0e6a04d3b7e6563a85
Kenneth <kltcalamay@gmail.com>
2010-09-30 23:36:48 +0800

    Wrote a failing step definition for the fourth pending step.

--- features/step_definitions/anonymous_steps.rb
+++ features/step_definitions/anonymous_steps.rb
@@ -10,3 +10,27 @@ end
 When /^I visit "([^"]*)"$/ do |url|
   visit "http://#{url}:3000"
 end
+
+Then /^I must see "([^"]*)"$/ do |expected_outcome|
+  case expected_outcome
+  when /an opt\-in page for the product named (.*)/
+    steps %q{
+      Then I should see "Firstname"
+      Then I should see "Email"
+      Then I should see "Phone"
+      Then I should see "#{$1}"
+    }
+
+  when /a sales page for the product named (.*)/
+    steps %q{
+      Then I should see "Buy now"
+      Then I should see "#{$1}"
+    }
+
+  when /the login page/
+    steps %q{
+      Then I should see "Username"
+      Then I should see "Password"
+    }
+  end
+end
