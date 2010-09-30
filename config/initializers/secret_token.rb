871e25c235f641c2e70b122699576ec5339f1969
Kenneth <kltcalamay@gmail.com>
2010-09-30 11:30:23 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProspectingSystem::Application.config.secret_token = '04cff8224876e92ef52fe92dcf6e706e9fea2dae57c559d07050a3933c7603bca9b324982f8aaa8cd2e5e1b3ac2765c6681d66d9cf2fe87c176f856d5c51d628'
