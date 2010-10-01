dab27d75b9593a8bcec90520d62bf83144b61d26
Kenneth <kltcalamay@gmail.com>
2010-10-01 12:17:48 +0800

    Modified app helper to handle empty product. CURRENT FEATURE DONE.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,9 @@
 module ApplicationHelper
   def title
-    # capitalize every word in product.name and return it as the title
-    @product.name.split(' ').each{ |s| s.capitalize! }.join(' ')
+    if @product
+      @product.name.split(' ').each{ |s| s.capitalize! }.join(' ')
+    else
+      "The Online Prospecting System for Filipino Network Marketers"
+    end
   end
 end
