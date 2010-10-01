f449222310843b381a111ca6e39673f31e2c33b4
Kenneth <kltcalamay@gmail.com>
2010-10-01 11:38:06 +0800

    Wired routes for the second time. Cucumber still not passing.

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,11 @@ ProspectingSystem::Application.routes.draw do
   resources :customers
   resources :prospects
 
-  products = Product.all
-  products.each do |p|
+  Product.all.each do |p|
     unless p.price > 0
       match '/' => 'prospects#new', :constraints => {:domain => p.domain}
+    else
+      match '/' => 'customers#index', :constraints => {:domain => p.domain}
     end
   end
 end
--- features/step_definitions/anonymous_steps.rb
+++ features/step_definitions/anonymous_steps.rb
@@ -4,12 +4,11 @@ Given /^the following products exist:$/ do |table|
   end
 
   ProspectingSystem::Application.routes.draw do
-    resources :prospects
-
-    products = Product.all
-    products.each do |p|
+    Product.all.each do |p|
       unless p.price > 0
         match '/' => 'prospects#new', :constraints => {:domain => p.domain}
+      else
+        match '/' => 'customers#index', :constraints => {:domain => p.domain}
       end
     end
   end
