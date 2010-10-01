3f67f33029ac0a4799e27cd6e6dfe1d46d437be4
Kenneth <kltcalamay@gmail.com>
2010-10-01 14:38:35 +0800

    Fixed bug on 'routes.rb'

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,13 @@ ProspectingSystem::Application.routes.draw do
   resources :customers
   resources :prospects
 
-  Product.all.each do |p|
-    unless p.price > 0
-      match '/' => 'prospects#new', :constraints => {:domain => p.domain}
-    else
-      match '/' => 'customers#index', :constraints => {:domain => p.domain}
+  if defined?(Product)
+    Product.all.each do |p|
+      unless p.price > 0
+        match '/' => 'prospects#new', :constraints => {:domain => p.domain}
+      else
+        match '/' => 'customers#index', :constraints => {:domain => p.domain}
+      end
     end
   end
 
