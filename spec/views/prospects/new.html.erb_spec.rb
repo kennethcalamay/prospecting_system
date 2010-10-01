a2a062accdf30c14008b4c52fba6877c161f7197
Kenneth <kltcalamay@gmail.com>
2010-10-01 14:11:01 +0800

    Modified and passed spec for prospects/new.html.erb to accomodate new requirements.

--- spec/views/prospects/new.html.erb_spec.rb
+++ spec/views/prospects/new.html.erb_spec.rb
@@ -1,18 +1,40 @@
 require "spec_helper"
 
 describe "prospects/new.html.erb" do
-  it "contains 'Firstname'" do
-    render
-    rendered.should contain("Firstname")
+  let(:prospect) { mock_model('Prospect').as_new_record.as_null_object }
+
+  before do
+    assign(:prospect, prospect)
   end
 
-  it "contains 'Email'" do
+  it "renders form to capture a prospect's contact information" do
     render
-    rendered.should contain("Email")
+    rendered.should have_selector('form', :method => 'post', :action => prospects_path) do |form|
+      form.should have_selector('input', :type => 'submit')
+    end
   end
 
-  it "contains 'Phone'" do
+  it "renders labeled textfied for the prospect's firstname" do
     render
-    rendered.should contain("Phone")
+    rendered.should have_selector('form') do |form|
+      form.should have_selector('label', :content => 'Firstname')
+      form.should have_selector('input', :type => 'text', :name => 'prospect[firstname]')
+    end
+  end
+
+  it "renders labeled textfied for the prospect's email" do
+    render
+    rendered.should have_selector('form') do |form|
+      form.should have_selector('label', :content => 'Email')
+      form.should have_selector('input', :type => 'text', :name => 'prospect[email]')
+    end
+  end
+
+  it "renders labeled textfied for the prospect's phone" do
+    render
+    rendered.should have_selector('form') do |form|
+      form.should have_selector('label', :content => 'Phone')
+      form.should have_selector('input', :type => 'text', :name => 'prospect[phone]')
+    end
   end
 end
