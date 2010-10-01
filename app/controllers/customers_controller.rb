9ebf33cde2c56fb69a927bd47bb789d0a3a9767d
Kenneth <kltcalamay@gmail.com>
2010-10-01 11:40:58 +0800

    Modified spec and implementation of prospects_controller.rb to provide views the objects they need.

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,2 +1,5 @@
 class CustomersController < ApplicationController
+  def index
+    @product = Product.find_by_domain(request.domain)
+  end
 end
--- spec/controllers/customers_controller_spec.rb
+++ spec/controllers/customers_controller_spec.rb
@@ -2,9 +2,21 @@ require "spec_helper"
 
 describe CustomersController do
   describe "GET 'index'" do
-    it "returns success code" do
+    let(:product) { mock_model('Product') }
+
+    before do
+      Product.stub(:find_by_domain).and_return(product)
+      @request.host = "www.somedomain.com"
+    end
+
+    it "queries for the product associated with the request's domain name" do
+      Product.should_receive(:find_by_domain).with(@request.domain)
       get 'index'
-      response.should be_success
+    end
+
+    it "assigns @product" do
+      get 'index'
+      assigns(:product).should eq(product)
     end
   end
 end
