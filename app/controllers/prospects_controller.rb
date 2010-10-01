3094270715f9f1c3ab474831254acac5ab20f1f7
Kenneth <kltcalamay@gmail.com>
2010-10-01 11:12:57 +0800

    Modified spec and implementation of prospects_controller.rb to accomodate recent changes in views.

--- app/controllers/prospects_controller.rb
+++ app/controllers/prospects_controller.rb
@@ -1,2 +1,5 @@
 class ProspectsController < ApplicationController
+  def new
+    @product = Product.find_by_domain(request.domain)
+  end
 end
--- spec/controllers/prospects_controller_spec.rb
+++ spec/controllers/prospects_controller_spec.rb
@@ -2,10 +2,21 @@ require "spec_helper"
 
 describe ProspectsController do
   describe "GET 'new'" do
-    it "returns success code" do
+    let(:product) { mock_model('Product') }
+
+    before do
+      Product.stub(:find_by_domain).and_return(product)
+      @request.host = "www.somedomain.com"
+    end
+
+    it "queries for the product associated with the request's domain name" do
+      Product.should_receive(:find_by_domain).with(@request.domain)
       get 'new'
-      response.should be_success
+    end
+
+    it "assigns @product" do
+      get 'new'
+      assigns(:product).should eq(product)
     end
   end
 end
-
